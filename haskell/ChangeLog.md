c692965635b0d16a00ba8ad51debfceee8f28394
Charles Durham <cpdurham@protonmail.com>
2020-07-08 21:35:56 -0400

    removed old code

--- haskell/ChangeLog.md
+++
@@ -1,3 +0,0 @@
-# Changelog for epigenisys
-
-## Unreleased changes
--- haskell/src/Epigenisys/Worlds/CudaWorld/Internal.hs
+++
@@ -1,7 +0,0 @@
-module Epigenisys.Worlds.CudaWorld.Internal 
-    ( module Epigenisys.Worlds.CudaWorld.Internal.AST
-    , module Epigenisys.Worlds.CudaWorld.Internal.Types
-    )where
-
-import Epigenisys.Worlds.CudaWorld.Internal.AST
-import Epigenisys.Worlds.CudaWorld.Internal.Types
