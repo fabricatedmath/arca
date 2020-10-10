c0054dd257de2c5b9c625390a4d936ab11dee4e4
Charles Durham <cpdurham@protonmail.com>
2020-10-10 17:14:17 -0400

    added tf files

--- sandbox/genetic/main.cu
+++ sandbox/genetic/main.cu
@@ -9,6 +9,8 @@ using namespace cooperative_groups;
 
 const int numPolys = 32;
 
+// TODO: log scores to global mem
+
 __constant__ int c_poly[numPolys];
 
 const int pPoly[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31};
